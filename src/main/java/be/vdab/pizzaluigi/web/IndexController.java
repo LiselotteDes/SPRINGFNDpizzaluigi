d09e86a4ff9a3ca623d7a21e526f03a59fc2cdce
Liselotte <liselotte.des@gmail.com>
2018-02-19 14:04:19 +0100

    hfdst9 (1,2,3): Controller en JSP combineren

--- src/main/java/be/vdab/pizzaluigi/web/IndexController.java
+++ src/main/java/be/vdab/pizzaluigi/web/IndexController.java
@@ -1,11 +1,16 @@
 package be.vdab.pizzaluigi.web;
 import java.time.LocalTime;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 // Je tikt @RestController voor een class die dient als controller
-@RestController
+//@RestController
+/*
+ * Als een controller samenwerkt met een JSP om een browser request te verwerken,
+ * tik je voor de class @Controller in plaats van @RestController.
+ */
+@Controller
 /*
  * Je associeert met @RequestMapping de controller met een URL in je website.
  * Dit is hier de URL die staat voor de welkompagina.
@@ -26,14 +31,22 @@ class IndexController {
 	 * De method moet niet public zijn, ze mag ook package visibility hebben.
 	 */
 	String index() {
-		int uur = LocalTime.now().getHour();
-		if (uur < 12) {
-			// De returnwaarde van de method is de response die Spring naar de browser terugstuurt.
-			return "Goede morgen";
-		}
-		if (uur < 18) {
-			return "Goede middag";
-		}
-		return "Goede avond";
+//		int uur = LocalTime.now().getHour();
+//		if (uur < 12) {
+//			// De returnwaarde van de method is de response die Spring naar de browser terugstuurt.
+//			return "Goede morgen";
+//		}
+//		if (uur < 18) {
+//			return "Goede middag";
+//		}
+//		return "Goede avond";
+		
+		// *** Controller en JSP combineren ***
+		
+		/*
+		 * De returnwaarde van de method, die de browser request verwerkt,
+		 * is de plaats en de naam van de JSP die de response naar de browser zal sturen.
+		 */
+		return "/WEB-INF/JSP/index.jsp";
 	}
 }
