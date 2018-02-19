b10e6b109873ed7559bc3fd50a8cc043d268bba1
Liselotte <liselotte.des@gmail.com>
2018-02-19 11:06:03 +0100

    html met titel, icoon, css

---
+++ src/main/java/be/vdab/pizzaluigi/PizzaluigiApplication.java
@@ -0,0 +1,12 @@
+package be.vdab.pizzaluigi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaluigiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaluigiApplication.class, args);
+	}
+}
