0dff8a8a8d7a7a5b6bfed3e97481f910eda51b9d
Liselotte <liselotte.des@gmail.com>
2018-03-07 08:47:21 +0100

    hfdst 27: Een integration test van een Spring bean

--- src/test/java/be/vdab/pizzaluigi/restclients/ECBKoersClientTest.java
+++ src/test/java/be/vdab/pizzaluigi/restclients/ECBKoersClientTest.java
@@ -4,15 +4,17 @@ import static org.junit.Assert.assertTrue;
 
 import java.math.BigDecimal;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 // een integration test voor de class ECBKoersClient
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ECBKoersClientTest {
+	@Autowired
 	private ECBKoersClient client;
-	@Before
-	public void before() {
-		client = new ECBKoersClient();
-	}
 	@Test
 	public void deKoersMoetPositiefZijn() {
 		assertTrue(client.getDollarKoers().compareTo(BigDecimal.ZERO) > 0);
--- src/test/java/be/vdab/pizzaluigi/restclients/FixerKoersClientTest.java
+++ src/test/java/be/vdab/pizzaluigi/restclients/FixerKoersClientTest.java
@@ -1,18 +1,29 @@
 package be.vdab.pizzaluigi.restclients;
 
 import static org.junit.Assert.*;
-
 import java.math.BigDecimal;
-
-import org.junit.Before;
+//import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+// Laat JUnit en Spring samenwerken
+@RunWith(SpringRunner.class)
+// Laat Spring zijn IOC container (met daarin de Spring beans) opbouwen
+@SpringBootTest
 public class FixerKoersClientTest {
+	/*
+	 * Als je @Autowired tikt voor een private variabele, zoekt Spring een bean waarvan de class dezelfde is als deze van die variabele.
+	 * Als Spring zo'n bean vindt, vult Spring de variabele met deze bean.
+	 */
+	@Autowired
 	private FixerKoersClient client;
-	@Before
-	public void before() {
-		client = new FixerKoersClient();
-	}
+//	@Before
+//	public void before() {
+//		client = new FixerKoersClient();
+//	}
 	@Test
 	public void deKoersMoetPositiefZijn() {
 		assertTrue(client.getDollarKoers().compareTo(BigDecimal.ZERO) > 0);
